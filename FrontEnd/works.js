7025a2a22718945f1f5438ed70e5daf93b7c056d
Natacha-R <166326801+Natacha-R@users.noreply.github.com>
2024-06-04 21:15:03 +0200

    rassemblement des fichiers JS (works/categories) et ajout unshift ligne 34

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,6 +1,20 @@
+let categories = [];
+
+// Récupération des données categories avec Fetch
+const request = async () => {
+  await fetch("http://localhost:5678/api/categories")
+    .then((response) => response.json())
+    .then((data) => {
+      categories = data;
+    })
+    .catch(function (error) {
+      alert("HTTP Error" + error.status);
+    });
+};
+
 let works = [];
 
-// Récupération des données avec Fetch
+// Récupération des données works avec Fetch
 const requestWorks = async () => {
   await fetch("http://localhost:5678/api/works")
     .then((response) => response.json())
@@ -12,12 +26,32 @@ const requestWorks = async () => {
     });
 };
 
+// Ajout des filtres
+const categoriesList = async () => {
+  await request();
+
+  const filtres = document.querySelector(".navbar");
+  categories.unshift({ id: 0, name: "Tous" });
+
+  for (const category of categories) {
+    const li = document.createElement("li");
+
+    li.textContent = category.name;
+    filtres.appendChild(li);
+  }
+};
+
+categoriesList();
+
 // Ajout des travaux "Mes Projets"
 const worksList = async () => {
   await requestWorks();
 
+  const appt = works.filter((work) => work.category.name === "Objets");
+  addWorks(appt);
+};
+function addWorks(works) {
   const section = document.querySelector(".gallery");
-
   for (const work of works) {
     const myFigure = document.createElement("figure");
     const myImg = document.createElement("img");
@@ -29,6 +63,5 @@ const worksList = async () => {
     myFigure.appendChild(myFigcaption);
     section.appendChild(myFigure);
   }
-};
-
+}
 worksList();
