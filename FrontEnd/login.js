3191d17efdb56de74023b38774a926dc35f0c57b
Natacha-R <166326801+Natacha-R@users.noreply.github.com>
2024-06-12 20:58:03 +0200

    modification lien href  page login et css login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,3 +1,25 @@
+UserLogin = [];
+
+// Récupération des données users/login avec Fetch
+const request = async () => {
+  await fetch("http://localhost:5678/api/users/login")
+    .then((response) => response.json())
+    .then((data) => {
+      UserLogin = data;
+    })
+    .catch(function (error) {
+      alert("HTTP Error" + error.status);
+    });
+};
+
+/*
 let inputEmail = document.querySelector("#email");
 let inputPassword = document.querySelector("#passWord");
 let BtnCo = document.querySelector("#btnCo");
+
+BtnCo.addEventListener("click", () => {
+  if (inputEmail.value < 1 || inputPassword < 1) {
+    console.log("stop !");
+  }
+});
+*/
