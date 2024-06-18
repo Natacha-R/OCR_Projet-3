b827427a4833d95744331acbc8feee29515971de
Natacha-R <166326801+Natacha-R@users.noreply.github.com>
2024-06-18 19:14:12 +0200

    Ajout fonction validateemail ligne 41/43 et 53/55

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -23,7 +23,7 @@ const request = async (bodyString) => {
 
       if (statusCode == 200) {
         sessionStorage.setItem("userId", UserLogin["userId"]); // enregistrer dans la session du site
-        sessionStorage.setItem("token", UserLogin["token"]);
+        sessionStorage.setItem("token", UserLogin["token"]); // stocker le token
 
         window.location.href = "index.html"; //rediriger vers la page d'accueil
       }
@@ -33,11 +33,15 @@ const request = async (bodyString) => {
     });
 };
 
+function validateEmail(email) {
+  const emailRegex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+  return emailRegex.test(email);
+}
+
 // Tentative de connexion utilisateur
 let inputEmail = document.querySelector("#email");
 let inputPassword = document.querySelector("#passWord");
 let BtnCo = document.querySelector("#btnCo");
-
 
 BtnCo.addEventListener("click", (event) => {
   event.preventDefault(); // Bloquer le comportement par defaut du navigateur
@@ -46,11 +50,11 @@ BtnCo.addEventListener("click", (event) => {
 
   if (mail.length == 0 && password.length == 0) {
     //si aucune saisie dans mail ou password
-    alert("Merci de saisir l'utilisateur et le mot de passe !");
-  } else if (mail.length == 0) {
-    alert("Merci de saisir l'utilisateur !");
+    alert("Erreur dans l'identifiant ou le mot de passe");
+  } else if (!validateEmail(mail)) {
+    alert("Erreur identifiant, mail non conforme");
   } else if (password.length == 0) {
-    alert("Merci de saisir le mot de passe !");
+    alert("Erreur mot de passe");
   } else {
     //sinon, création du contenu de la requette
     const body = { email: mail, password: password }; //création du dictionnaire contenant les saisies utilisateur
