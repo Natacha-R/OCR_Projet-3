8b27d4e18947f256529206e2be826dbb16fa94ec
Natacha-R <166326801+Natacha-R@users.noreply.github.com>
2024-06-24 22:56:12 +0200

    Logout + première version modale

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -58,13 +58,42 @@ const categoriesList = async () => {
     const p = document.createElement("p");
     p.textContent = "modifier";
     const i = document.createElement("i");
-    i.classList.add("fa-regular", "fa-pen-to-square");
+    i.classList.add("fa-regular", "fa-pen-to-square"); //icone Fontawesome
     const modify = document.getElementById("modify");
     modify.appendChild(i);
     modify.appendChild(p);
 
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    modify.addEventListener("click", (event) => {
+      event.preventDefault();
+      modal.style.display = "flex";
+    });
+
+    // When the user clicks on <span> (x), close the modal
+    span.addEventListener("click", (event) => {
+      modal.style.display = "none";
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    };
+
     // changement login en logout
-    const logout = (document.querySelector(".logac").innerText = "logout");
+    document.querySelector(".logac").innerText = "logout";
+
+    const logoutButton = document.querySelector(".logac");
+    logoutButton.addEventListener("click", () => {
+      sessionStorage.removeItem("token");
+      window.location.href = "login.html";
+    });
   }
 };
 
@@ -75,6 +104,7 @@ const worksList = async () => {
   await requestWorks();
 
   const section = document.querySelector(".gallery");
+  const grid = document.getElementById("grid");
 
   for (const work of works) {
     const myFigure = document.createElement("figure");
@@ -86,6 +116,23 @@ const worksList = async () => {
     section.appendChild(myFigure);
     myFigure.appendChild(myImg);
     myFigure.appendChild(myFigcaption);
+
+    const galleryImg = document.createElement("img");
+    galleryImg.src = work.imageUrl;
+    grid.appendChild(galleryImg);
+  }
+  /*
+  Pour avoir le nombre de lignes requises : nombre de projets (works) / 5 (nombre projet par ligne)
+  On multiplie par 100 car la hauteur de nos images est de 100
+  On rajoute 55 car c'est l'espacement requis par la maquette
+  On transforme le résultat en chaine de caractère car style.height attend ce type
+  On ajoute px car on attend que notre grille ait une hauteur en pixels
+  */
+  const height = Math.ceil(works.length / 5) * 100 + 55;
+  grid.style.height = height.toString() + "px";
+  if (height > 355) {
+    //Si hauteur images > 355 alors on scroll
+    grid.style.overflow = "auto";
   }
 };
 worksList();
